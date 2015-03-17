e0abc296bc31c246e90bcc7e4c45c4d883882b27
ridzuan <ridz0n391@gmail.com>
2015-03-17 09:43:08 +0800

    pelaporan update

---
+++ www/js/CheckBoxPelaporan.js
@@ -0,0 +1,158 @@
+		$("#c_box1").click(function() 
+		{
+			if($("#c_box1").is(':checked'))
+			{
+				$("#c_box2").prop( "checked", true );
+				$("#c_box3").prop( "checked", true );
+				$("#c_box4").prop( "checked", true );
+				$("#c_box5").prop( "checked", true );
+				$("#c_box6").prop( "checked", true );
+				$("#c_box7").prop( "checked", true );
+				$("#c_box8").prop( "checked", true );
+				$("#c_box9").prop( "checked", true );
+				$("#c_box10").prop( "checked", true );
+				$("#c_box11").prop( "checked", true );
+				$("#c_box12").prop( "checked", true );
+				$("#c_box13").prop( "checked", true );
+				$("#c_box14").prop( "checked", true );
+				$("#c_box15").prop( "checked", true );
+				$("#c_box16").prop( "checked", true );
+			}else
+			{
+				$("#c_box2").removeAttr('checked');
+				$("#c_box3").removeAttr('checked');
+				$("#c_box4").removeAttr('checked');
+				$("#c_box5").removeAttr('checked');
+				$("#c_box6").removeAttr('checked');
+				$("#c_box7").removeAttr('checked');
+				$("#c_box8").removeAttr('checked');
+				$("#c_box9").removeAttr('checked');
+				$("#c_box10").removeAttr('checked');
+				$("#c_box11").removeAttr('checked');
+				$("#c_box12").removeAttr('checked');
+				$("#c_box13").removeAttr('checked');
+				$("#c_box14").removeAttr('checked');
+				$("#c_box15").removeAttr('checked');
+				$("#c_box16").removeAttr('checked');
+			}
+		});
+
+		$("#c_box2").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box3").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box4").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box5").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box6").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box7").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box8").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box9").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box10").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box11").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box12").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box13").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box14").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box15").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
+
+		$("#c_box16").click(function() 
+		{
+			if($("#c_box1").is(":checked"))
+			{
+				$("#c_box1").prop("checked", false);
+			}
+		});
