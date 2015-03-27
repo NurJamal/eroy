1be3f41a9abcbcf374db0aa9d36c79ec8d494852
Imal Pasha <imalpasha@gmail.com>
2015-03-27 17:33:39 +0800

    no message

--- www/js/internal/custom.js
+++ www/js/internal/custom.js
@@ -18,91 +18,52 @@
 			}
 			}); 
 		
+		if(window.localStorage.getItem("_MENU_FIX") == "FALSE"){
+			/*Slide to Hide Header Menu*/
+			// Hide Header on on scroll down
+			var didScroll;
+			var lastScrollTop = 0;
+			var delta = 120;
+			var navbarHeight = $("#head").outerHeight();
 
-		/*Slide to Hide Header Menu*/
-		// Hide Header on on scroll down
-		var didScroll;
-		var lastScrollTop = 0;
-		var delta = 120;
-		var navbarHeight = $("#head").outerHeight();
+			$(window).scroll(function(event){
+				didScroll = true;
+			});
 
-		$(window).scroll(function(event){
-			didScroll = true;
-		});
+			setInterval(function() {
+				if (didScroll) {
+					hasScrolled();
+					didScroll = false;
+				}
+			}, 450);
 
-		setInterval(function() {
-			if (didScroll) {
-				hasScrolled();
-				didScroll = false;
-			}
-		}, 450);
-
-		function hasScrolled() {
-		var st = $(this).scrollTop();
-		
-		// Make sure they scroll more than delta
-		if(Math.abs(lastScrollTop - st) <= delta)
-			return;
-		
-		// If they scrolled down and are past the navbar, add class .nav-up.
-		// This is necessary so you never see what is "behind" the navbar.
-		if (st > lastScrollTop && st > navbarHeight){
-			// Scroll Down
-			/*Put in custom class later nav-down*/
 			
+			function hasScrolled() {
+			var st = $(this).scrollTop();
 			
-				//$("#head").css("top","-80px");
-				//tak smooth la pulak
+			// Make sure they scroll more than delta
+			if(Math.abs(lastScrollTop - st) <= delta)
+				return;
 			
-				
-				//$("#head").css("transform","translate3d( -300px, 0, 0 )").animate({top:-80px}, "fast");
-				//$("#head").css("transform", "translate3d( -300px, 0, 0 )");					
+			// If they scrolled down and are past the navbar, add class .nav-up.
+			// This is necessary so you never see what is "behind" the navbar.
+			if (st > lastScrollTop && st > navbarHeight){
+				// Scroll Down
+				/*Put in custom class later nav-down*/
 				$("#head").animate({opacity: '0.0',},"slow");					
 
-				//$(".navbar").css("top","0");
-				//$(".navbar").css("position","fixed");
-				//$(".navbar").css("background-color","#F2F1EF");
-				//$(".navbar").css("padding-top","12px");
-				//$(".navbar").css("z-index","9");
-				//$("._form").css("padding-top","83px");
-				//$(".navbar").css("width","100%");
-				//$(".navbar").css("height","auto");
-				//$(".content_title").css("border-bottom","0px");
-				
-				//$(".navbar").css("border-bottom","2px solid #F9690E");
-				//$("._form").css("padding-left","9px");
-				//$("._form").css("padding-right","9px");
-				//$(".navbar").css("padding-left","5px");
-				//$(".navbar").css("padding-right","5px");
-				//$(".content_title").css("position","fixed");
-				//$(".content_title").css("top","0px");
-
-		} else {
-			// Scroll Up
-			/*Put in custom class later nav-up*/
-
-			if(st + $(window).height() < $(document).height()) {
-				//$("#head").css("top","0");
-				//$("#head").animate({top: '0'}, "fast");
-				
-				//$("#head").animate({top: '0'}, "fast");					
+			} else {
+				// Scroll Up
+				/*Put in custom class later nav-up*/
+				if(st + $(window).height() < $(document).height()) {
+						
 				$("#head").animate({opacity: '1'},"fast");	
-				//$(".navbar").css("top","");
-				//$(".navbar").css("position","");
-				//$(".navbar").css("padding-top","");
-				//$(".navbar").css("z-index","");
 				
-				
-				//$("._form").css("padding-top","");
-
-				//$(".navbar").css("padding-left","");
-				//$("._form").css("padding-left","");
-				//$("._form").css("padding-right","");
-				//$(".content_title").css("border-bottom","2px solid #F9690E");
+				}
 			}
+			
+			lastScrollTop = st;
 		}
-		
-		lastScrollTop = st;
 	}
 
 
@@ -153,7 +114,6 @@
 	else
 	{
 		$("#navigation").load('navigation_menu/nav_login.html');
-	
 	}
 
 	/* Refresh Captcha  */
